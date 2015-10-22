ad87a7adf1772c62276aafa00303611784a34745
bbatta38 <bbatta38@gmail.com>
2015-10-22 15:09:47 +0900

    add sort table source

--- assets/js/table_yh.js
+++ assets/js/table_yh.js
@@ -1,26 +1,4 @@
-$(document).ready(function(){
-	var rows = [];
-	$(".sb-yh-table tbody tr").each(function(i){
-		var cols = [];
-		//insertTd($(this));
-		$(this).find("td").each(function(j){
-			cols[j] = $(this).html();
-		});
-		rows.push(cols);
-	});
-	console.log(rows);
-
-	// function insertTd(data) {
-		
-	// }
-
-	// var dataTable = {
-	// 	data: [],
-	// 	init: function() {
-	// 		$('')
-	// 	}
-
-	// }
-
-	// dataTable.init();
+$(function(){
+	var table = $(".sb-sort-table").sortTable({
+	});	
 });
